746b3d8aba041008d01110d12cac092407557d8c
Ferny04 <farfanerique095@gmail.com>
2026-09-10 19:29:42 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //control para mostrar u ocultar la contraseña
+  bool _obscure = true;
   @override
   Widget build(BuildContext context) {
     // Para obtener el tamaño de la pantalla
@@ -23,6 +25,48 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: size.width,
                 height: 200,
                 child: const RiveAnimation.asset('assets/login_bear.riv'),
+              ),
+              //para separar espacios
+              SizedBox(height: 10),
+              //Campo de texto para Email
+              TextField(
+                //para mostrar el tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration:InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    //para redondear los bordes
+                    borderRadius: BorderRadius.circular(12),
+                  )
+                )
+              ),
+              SizedBox(height: 10),
+              //Campo de texto para contraseña
+              TextField(
+                obscureText: _obscure,
+                //para mostrar el tipo de teclado
+                keyboardType: TextInputType.visiblePassword,
+                decoration:InputDecoration(
+                  hintText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    //if ternario
+                    icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off
+                    ),
+                    onPressed: (){
+                      //Refrescar el icono de la contraseña
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    //para redondear los bordes
+                    borderRadius: BorderRadius.circular(12),
+                  )
+                )
               ),
             ],
           ),
